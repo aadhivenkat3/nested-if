45ccc9e282448ca21434a733db6f935129fa64a2
aadhivenkat3 <sharmavenkat1@gmail.com>
2023-07-20 16:50:37 +0530

    add

---
+++ main.js
@@ -0,0 +1,19 @@
+// js condition nested if
+
+var flower=prompt("enter your favourite flower");
+
+	if(flower=="rose"){
+		document.write("rose");
+	}
+		else if(flower=="lilly"){
+			document.write("lilly");
+		}
+		else if(flower=="jasmine"){
+			document.write("jasmine");
+		}
+		else if(flower=="red rose"){
+			document.write("red rose");
+		}
+	else{
+		document.write("no flower");
+	}
